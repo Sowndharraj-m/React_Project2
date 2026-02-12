1a2b352382847680661c3cf747c4b658ea6b18b9
Your Name <your.email@example.com>
2026-02-12 19:07:17 +0530

    Status_ Upadted

--- vite-project/src/Pages/Homepage.jsx
+++ vite-project/src/Pages/Homepage.jsx
@@ -1,7 +1,8 @@
-
+import { useState } from "react"
 import MovieCard from "../components/MovieCard"
 
 function Homepage() {
+   const [search, setSearch] = useState("")
    const movies = [
       { id: 1, title: "RRR", releaseDate: 2020 },
       { id: 2, title: "Master", releaseDate: 2021 },
@@ -10,13 +11,24 @@ function Homepage() {
       { id: 5, title: "Jailer", releaseDate: 2024 }
    ]
 
-   return <div className="Homepage">
-      <div>
-         <div className="movie-grid">
-            {movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+   const handlesearch = (e) => {
+      e.preventDefault()
+      console.log(search)
+      setSearch("")
+   }
+
+   return (
+      <div className="Homepage">
+         <form onSubmit={handlesearch} className="search-form">
+            <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+            <button type="submit">Search</button>
+         </form>
+         
+            <div className="movie-grid">
+               {movies.map((movie) => movie.title.toLowerCase().includes(search.toLowerCase()) && <MovieCard key={movie.id} movie={movie} />)}
+            </div>
          </div>
-      </div>
-   </div>
+   )
 }
 
 export default Homepage
